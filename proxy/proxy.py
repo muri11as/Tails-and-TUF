4aa31df9872835ba8bcc7842263e678bc720f1bd
kev5873 <techiekevin@gmail.com>
2013-11-11 01:43:46 -0500

    Starting point for web proxy

---
+++ proxy/proxy.py
@@ -0,0 +1,25 @@
+import web
+import tuf
+import tuf.interposition
+import tuf.log
+from tuf.interposition import urllib2_tuf as urllib2
+		
+urls = (
+	'/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+	def GET(self, name):
+		tuf.log.add_console_handler()
+		try:
+			tuf.interposition.configure(filename="tuf.interposition.json")
+		except tuf.Error, error:
+			return 'TUF could not initialize due to an error: '+str(error)
+
+		url = 'http://www.bvestation.com/tuf'
+		# user_data = web.input(link)
+		urllib2.urlopen(url)
+
+if __name__ == "__main__":
+	app.run()
