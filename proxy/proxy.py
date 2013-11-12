83b85bad6b8473e6545e22874c0584ab5bfb1eaf
kev5873 <techiekevin@gmail.com>
2013-11-12 00:01:56 -0500

    Proxy server

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -11,15 +11,15 @@ app = web.application(urls, globals())
 
 class hello:
 	def GET(self, name):
-		tuf.log.add_console_handler()
-		try:
-			tuf.interposition.configure(filename="tuf.interposition.json")
-		except tuf.Error, error:
-			return 'TUF could not initialize due to an error: '+str(error)
+		# tuf.log.add_console_handler()
+		# try:
+		# 	tuf.interposition.configure(filename="tuf.interposition.json")
+		# except tuf.Error, error:
+		# 	return 'TUF could not initialize due to an error: '+str(error)
 
-		url = 'http://www.bvestation.com/tuf'
-		# user_data = web.input(link)
-		urllib2.urlopen(url)
+		# url = 'http://www.bvestation.com/tuf'
+		user_data = web.input()
+		return urllib2.urlopen(user_data.link)
 
 if __name__ == "__main__":
 	app.run()
