33c2edd06b70e4dc422680cdb06cba479af9a711
kev5873 <techiekevin@gmail.com>
2013-11-26 01:32:09 -0500

    added time difference

--- AttackDemos/SlowRetrieval/slow.sh
+++ AttackDemos/SlowRetrieval/slow.sh
@@ -5,7 +5,9 @@
 #Description: Use a bandwidth shaping tool (SpeedLimit on mac, NetLimiter on win)
 #             to slow down the speeds on a Tails VM.
 ############################################################################
-date
+START=$(date +%s)
 tails-iuk-get-target-file --uri http://www.toannv.com/tuf/targets/Tails_i386_0.21_to_0.22.iuk --hash-type sha256 --hash-value 277db6d7371d82241603f5f87d124c6087050703caa951b22c8f37ed6cf31037 --output-file tails.iuk --size 184320
 echo "Tails_i386_0.21_to_0.22.iuk, should've been downloaded into the temporary directory"
-date
+END=$(date +%s)
+DIFF=$(( $END - $START ))
+echo "Operation completed in $DIFF seconds"
