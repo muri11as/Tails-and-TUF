0bb550c8de6e331c389503e9b64762e25d93f1d9
kev5873 <techiekevin@gmail.com>
2013-11-26 18:12:04 -0500

    ok

--- AttackDemos/SlowRetrieval/slowWithTUF.sh
+++ AttackDemos/SlowRetrieval/slowWithTUF.sh
@@ -6,7 +6,7 @@
 #             to slow down the speeds on a Tails VM.
 ############################################################################
 START=$(date +%s)
-tails-iuk-get-target-file --uri http://dl.amnesia.boum.org/Tails_i386_0.21_to_0.22-1.iuk --hash-type sha256 --hash-value fc14986953d085d1240905bf64d511b5bf21491d795912bb5ebd5ef779850bb1 --output-file Tails_i386_0.21_to_0.22.iuk --size 18341264
+tails-iuk-get-target-file --uri http://dl.amnesia.boum.org/Tails_i386_0.21_to_0.22.iuk --hash-type sha256 --hash-value fc14986953d085d1240905bf64d511b5bf21491d795912bb5ebd5ef779850bb1 --output-file Tails_i386_0.21_to_0.22.iuk --size 18341264
 echo "Tails_i386_0.21_to_0.22.iuk, should've been downloaded into the temporary directory"
 END=$(date +%s)
 DIFF=$(( $END - $START ))
