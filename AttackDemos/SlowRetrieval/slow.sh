596195ee3ea7d11b9edee1abb6f2a385677b4dec
kev5873 <techiekevin@gmail.com>
2013-11-26 16:37:04 -0500

    Change of link

--- AttackDemos/SlowRetrieval/slow.sh
+++ AttackDemos/SlowRetrieval/slow.sh
@@ -6,7 +6,7 @@
 #             to slow down the speeds on a Tails VM.
 ############################################################################
 START=$(date +%s)
-tails-iuk-get-target-file --uri http://dl.amnesia.boum.org/Tails_i386_0.21_to_0.22.iuk --hash-type sha256 --hash-value 277db6d7371d82241603f5f87d124c6087050703caa951b22c8f37ed6cf31037 --output-file Tails_i386_0.21_to_0.22.iuk --size 184320
+tails-iuk-get-target-file --uri http://toannv.com/tuf/targets/Tails_i386_0.21_to_0.22.iuk --hash-type sha256 --hash-value fc14986953d085d1240905bf64d511b5bf21491d795912bb5ebd5ef779850bb1 --output-file Tails_i386_0.21_to_0.22.iuk --size 18341264
 echo "Tails_i386_0.21_to_0.22.iuk, should've been downloaded into the temporary directory"
 END=$(date +%s)
 DIFF=$(( $END - $START ))
