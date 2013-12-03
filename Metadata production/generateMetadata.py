7750f716a4d16b7b531b350e24fc83075b22432c
muri11as <cesar.murillas11@gmail.com>
2013-12-03 01:11:13 -0500

    Added config for repo generation

--- Metadata production/generateMetadata.py
+++ Metadata production/generateMetadata.py
@@ -4,16 +4,30 @@ Author: Cesar Murillas
 import os 
 from tuf.libtuf import *
 
+##PARSE CONFIG FILE FOR ROOTPATH, PATH AND PASSWORD
+rootkey,repoName,keystore,pword = '','','',''
+
+filey = open("/Users/Ceeze/Desktop/repoconfig.txt",'r')
+for line in filey:
+	liss= line.split('=',1)
+	if liss[0] == "ROOTKEY":
+		rootkey = liss[1].strip()
+	elif liss[0] == "REPONAME":
+		repoName = liss[1].strip()
+	elif liss[0] == "KEYSTORE":
+		keystore = liss[1].strip()
+	elif liss[0] == "PASSWORD":
+		pword = liss[1].strip()
+filey.close()
+
 ##IMPORT RSA KEYS
 
-public_root_key = import_rsa_publickey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/rootkeys/root_key1.pub")
-private_root_key = import_rsa_privatekey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/rootkeys/root_key1",password="gogreen")
+public_root_key = import_rsa_publickey_from_file(rootkey+".pub")
+private_root_key = import_rsa_privatekey_from_file(rootkey,password=pword)
 
-
-##CREATE ROOT
 
 #CREATE NEW REPOSITORY
-repository = create_new_repository("/Users/Ceeze/Desktop/assignment3.3/tuf")
+repository = create_new_repository(repoName)
 
 repository.root.add_key(public_root_key)
 repository.root.threshold = 1
@@ -22,9 +36,11 @@ repository.root.load_signing_key(private_root_key)
 ##CREATE TIMESTAMP, RELEASE, && TARGETS ROLES
 
 #ADD PUBLIC KEYS
-repository.timestamp.add_key(import_rsa_publickey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/timestamp.pub"))
-repository.release.add_key(import_rsa_publickey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/release.pub"))
-repository.targets.add_key(import_rsa_publickey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/targets.pub"))
+path = repoName+keystore
+
+repository.timestamp.add_key(import_rsa_publickey_from_file(path+"timestamp.pub"))
+repository.release.add_key(import_rsa_publickey_from_file(path+"release.pub"))
+repository.targets.add_key(import_rsa_publickey_from_file(path+"targets.pub"))
 
 #SET THRESHOLDS
 repository.timestamp.threshold = 1
@@ -32,9 +48,9 @@ repository.release.threshold = 1
 repository.targets.threshold = 1
 
 #IMPORT SIGNING KEYS
-private_timestamp_key = import_rsa_privatekey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/timestamp",password="gogreen")
-private_release_key = import_rsa_privatekey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/release",password="gogreen")
-private_targets_key = import_rsa_privatekey_from_file("/Users/Ceeze/Desktop/assignment3.3/tuf/keys/targets",password="gogreen")
+private_timestamp_key = import_rsa_privatekey_from_file(path+"timestamp",password=pword)
+private_release_key = import_rsa_privatekey_from_file(path+"release",password=pword)
+private_targets_key = import_rsa_privatekey_from_file(path+"targets",password=pword)
 #LOAD KEYS
 repository.timestamp.load_signing_key(private_timestamp_key)
 repository.release.load_signing_key(private_release_key)
