14f6c5f47b8b2b9def40c3df8be69df4884c0d5a
Toan Van Nguyen <toanvannguyen@outlook.com>
2013-12-03 01:48:58 -0500

    Add different passwords for different key roles

--- Metadata production/generateKeyStore.py
+++ Metadata production/generateKeyStore.py
@@ -1,30 +1,43 @@
 '''
 Author: Cesar Murillas
 CS9163
-Key Creation
+Modified: Toan Nguyen
+Description: Key creation script. Will be used for first-time creation 
+of TUF keys or after revocation
+Run: python generateKeyStore.py path_to_config_file
 '''
-
+import sys
 from tuf.libtuf import *
 
 ##PARSE CONFIG FILE FOR ROOTPATH, PATH AND PASSWORD
-rootpath,path,pword = '','',''
-
-filey = open("/Users/Ceeze/Desktop/keyconfig.txt",'r')
+rootpath,path = '',''
+rootpwd,targetpwd,releasepwd,timestamppwd = '','','',''
+try:
+	filey = open(sys.argv[1],'r')
+except:
+	print "Can't read config file!"
+	sys.exit(1)
 for line in filey:
 	liss= line.split('=',1)
 	if liss[0] == "ROOTKEYSTORE":
 		rootpath = liss[1].strip()
 	elif liss[0] == "KEYSTORE":
 		path = liss[1].strip()
-	elif liss[0] == "PASSWORD":
-		pword = liss[1].strip()
+	elif liss[0] == "ROOTPASSWORD":
+		rootpwd = liss[1].strip()
+	elif liss[0] == "TARGETPASSWORD":
+		targetpwd = liss[1].strip()
+	elif liss[0] == "RELEASEPASSWORD":
+		releasepwd = liss[1].strip()
+	elif liss[0] == "TIMESTAMPPASSWORD":
+		timestamppwd = liss[1].strip()
 filey.close()	 
 ##CREATE AND STORE ROOT RSA KEY PAIR
 
-generate_and_write_rsa_keypair(rootpath+"root_key",bits=3072,password=pword)
+generate_and_write_rsa_keypair(rootpath+"root_key",bits=3072,password=rootpwd)
 
 ##CREATE AND STORE TIMESTAMP, RELEASE, && TARGETS PUBLIC && PRIVATE KEYS
 
-generate_and_write_rsa_keypair(path+"timestamp",bits=3072,password=pword)
-generate_and_write_rsa_keypair(path+"release",bits=3072,password=pword)
-generate_and_write_rsa_keypair(path+"targets",bits=3072,password=pword)
+generate_and_write_rsa_keypair(path+"timestamp",bits=3072,password=timestamppwd)
+generate_and_write_rsa_keypair(path+"release",bits=3072,password=releasepwd)
+generate_and_write_rsa_keypair(path+"targets",bits=3072,password=targetpwd)
