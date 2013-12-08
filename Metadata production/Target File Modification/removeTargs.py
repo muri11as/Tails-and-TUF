52a9660537ec61cfb5513527f7f28d410db69a35
muri11as <cesar.murillas11@gmail.com>
2013-12-08 04:44:19 -0500

    Basis for changing target metadata on the fly. Need to Branch out for each delegation. Have script be able to change whichever delegation necessary. Need to add that to targets/

--- Metadata production/Target File Modification/removeTargs.py
+++ Metadata production/Target File Modification/removeTargs.py
@@ -2,7 +2,7 @@
 
 AUTHOR: CESAR MURILLAS
 DESCRIPTION: THIS SCRIPT WILL ADD TARGET FILES TO TARGETS ROLE METADATA
-USAGE: run python addtargs.py path/to/targetsconfig.txt
+USAGE: run python removeTargs.py path/to/targetconfig.txt
 
 '''
 
@@ -49,53 +49,27 @@ for line in filey:
 		
 	elif liss[0] == "NIGHTLYPASSWORD":
 		del3pwd = liss[1].strip()
+		
 filey.close()
 
-##ADD TARGET FILES
-'''Make sure the target files are saved in the targets directory of the repository'''
 repository = load_repository(repoName)
 
-#Get list of file all file paths
-tList = repository.get_filepaths_in_directory(repoName+"targets", recursive_walk=True, followlinks=True)
-
-#Add List of target files to the targets metadata
-#for targ in tList:
-	#repository.targets.add_target(targ)
-
-#IMPORT DELEGATES' PUBLIC KEYS
-public_del1_key = import_rsa_publickey_from_file(keystore+"stable.pub")
-public_del2_key = import_rsa_publickey_from_file(keystore+"beta.pub")
-public_del3_key = import_rsa_publickey_from_file(keystore+"nightly.pub")
+#REMOVE ALL TARGETS
+repository.targets.revoke("stable")
+repository.targets.revoke("beta")
+repository.targets.revoke("nightly")
 
 #IMPORT SIGNING KEYS
-private_del1_key = import_rsa_privatekey_from_file(keystore+"stable",password=del1pwd)
-private_del2_key = import_rsa_privatekey_from_file(keystore+"beta",password=del2pwd)
-private_del3_key = import_rsa_privatekey_from_file(keystore+"nightly",password=del3pwd)
 private_root_key = import_rsa_privatekey_from_file(rkeystore+"root_key",password=rootpwd)
 private_timestamp_key = import_rsa_privatekey_from_file(keystore+"timestamp",password=timestamppwd)
 private_release_key = import_rsa_privatekey_from_file(keystore+"release",password=releasepwd)
 private_targets_key = import_rsa_privatekey_from_file(keystore+"targets",password=targetspwd)
 
-#DELEGATE
-repository.targets.delegate("stable",[public_del1_key], tList)
-repository.targets.stable.version = repository.targets.version+1
-
-repository.targets.delegate("beta",[public_del2_key], tList)
-repository.targets.beta.version = repository.targets.version+1
-
-repository.targets.delegate("nightly",[public_del3_key], tList)
-repository.targets.nightly.version = repository.targets.version+1
-
 #LOAD SIGNING KEYS
-repository.targets.stable.load_signing_key(private_del1_key)
-repository.targets.beta.load_signing_key(private_del2_key)
-repository.targets.nightly.load_signing_key(private_del3_key)
 repository.root.load_signing_key(private_root_key)
 repository.targets.load_signing_key(private_targets_key)
 repository.timestamp.load_signing_key(private_timestamp_key)
 repository.release.load_signing_key(private_release_key)
 
-
 #NEW VERSIONS OF METADATA
-
 repository.write()
